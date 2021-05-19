cc1bcfede702184215c9fb6483392bdd21e5821a
raman20 <ramandwivedi20@protonmail.com>
2021-05-19 18:45:05 +0530

    integrated compiler api

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http)
-const port = 3009;
+const port = 3505;
+const request = require("request");
 
+const codeObj = {
+    clientId: "3cfe4aad88fcf8ba5d0bcb5d742cf9d9",
+    clientSecret: "d9d9ff736afe347d80aaeb8882e822dec003109b381495fd01ce125381059df3"
+}
 
 app.set('view engine', 'ejs');
 
@@ -31,9 +36,28 @@ io.on('connection', (socket) => {
         log(e)
         socket.to(ROOM_ID).emit('message', e)
     });
+
+    socket.on('code run', (ROOM_ID, script, lang, version) => {
+        codeObj['script'] = script;
+        codeObj['language'] = lang;
+        codeObj['versionIndex'] = version;
+
+
+        console.table(codeObj);
+
+        request({
+            url: 'https://api.jdoodle.com/v1/execute',
+            method: "POST",
+            json: codeObj
+        },
+            function (error, response, body) {
+                console.table(body);
+                io.in(ROOM_ID).emit('code response', body);
+            });
+    })
 });
 
 
 io.on('disconnect', (evt) => {
-    log('some people left')
+    log('some people left');
 })
